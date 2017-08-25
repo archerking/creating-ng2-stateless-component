d4f79004b9748e20af0c067c4af6a6da72437ec6
Avnish <avnish@localhost.localdomain>
2017-08-25 17:26:19 -0400

    some code comments added

--- src/app/shared/stateless-comp/stateless-comp.component.ts
+++ src/app/shared/stateless-comp/stateless-comp.component.ts
@@ -6,7 +6,14 @@ import { Validators, FormGroup, FormControl } from '@angular/forms';
   templateUrl: './stateless-comp.component.html',
   styleUrls: ['./stateless-comp.component.css']
 })
+
 export class StatelessCompComponent implements OnInit {
+  /**
+   * Initializing 'addItem' as EventEmitter so that other components can listen events dispatched by the form.
+   * 
+   * @type {EventEmitter<any>}
+   * @memberof StatelessCompComponent
+   */
   @Output() addItem: EventEmitter<any> = new EventEmitter();
   private formModel: FormGroup;
   constructor() {
--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -11,8 +11,17 @@ export class HomeComponent {
   constructor() { 
   }
 
-  addToParent(e){
+  /**
+   * Capturing event emitted by 'stateless-comp'
+   * 
+   * @param {*} e 
+   * @memberof HomeComponent
+   */
+  addToParent(e:any):void{
     console.log(e.value);
+    /**
+     * Adding value into items.
+     */
     this.items.push(e.value);
   }
 
